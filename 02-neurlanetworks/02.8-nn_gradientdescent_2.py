41e0c6e5250e262833259816235820c5c4972bf3
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-06-05 02:42:14 +0200

    update nn with gradient descent code

---
+++ 02-neurlanetworks/02.8-nn_gradientdescent_2.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+
+import os
+cwd = os.getcwd()
+
+admissions = pd.read_csv("02-neurlanetworks/binary.csv")
+
+# Make dummy variables for rank
+data = pd.concat([admissions, pd.get_dummies(admissions['rank'], prefix='rank')], axis=1)
+data = data.drop('rank', axis=1)
+
+# Standarize features
+for field in ['gre', 'gpa']:
+    mean, std = data[field].mean(), data[field].std()
+    data.loc[:, field] = (data[field] - mean) / std
+
+# Split off random 10% of the data for testing
+np.random.seed(42)
+sample = np.random.choice(data.index, size=int(len(data) * 0.9), replace=False)
+data, test_data = data.iloc[sample], data.drop(sample)
+
+# Split into features and targets
+features, targets = data.drop('admit', axis=1), data['admit']
+features_test, targets_test = test_data.drop('admit', axis=1), test_data['admit']
+
+import numpy as np
+#from data_prep import features, targets, features_test, targets_test
+
+def sigmoid(x):
+    """
+    Calculate sigmoid
+    """
+    return 1 / (1 + np.exp(-x))
+
+# TODO: We haven't provided the sigmoid_prime function like we did in
+#       the previous lesson to encourage you to come up with a more
+#       efficient solution. If you need a hint, check out the comments
+#       in solution.py from the previous lecture.
+
+# Note: The sigmoid_prime function calculates sigmoid(h) twice,
+#       but you've already calculated it once. You can make this
+#       code more efficient by calculating the derivative directly
+#       rather than calling sigmoid_prime, like this:
+# error_term = error * nn_output * (1 - nn_output)
+
+
+# Use to same seed to make debugging easier
+np.random.seed(42)
+
+n_records, n_features = features.shape
+last_loss = None
+
+# Initialize weights
+weights = np.random.normal(scale=1 / n_features**.5, size=n_features)
+
+# Neural Network hyperparameters
+epochs = 1000
+learnrate = 0.5
+
+for e in range(epochs):
+    del_w = np.zeros(weights.shape)
+    for x, y in zip(features.values, targets):
+        # Loop through all records, x is the input, y is the target
+
+        # Note: We haven't included the h variable from the previous
+        #       lesson. You can add it if you want, or you can calculate
+        #       the h together with the output
+
+        # TODO: Calculate the output
+        output = sigmoid(x.dot(weights))
+
+        # TODO: Calculate the error
+        error = y - output
+
+        # TODO: Calculate the error term
+        error_term = error * output * (1-output)
+
+        # TODO: Calculate the change in weights for this sample
+        #       and add it to the total weight change
+        del_w += error_term * x
+
+    # TODO: Update weights using the learning rate and the average change in weights
+    weights += learnrate * del_w/n_records
+
+    # Printing out the mean square error on the training set
+    if e % (epochs / 10) == 0:
+        out = sigmoid(np.dot(features, weights))
+        loss = np.mean((out - targets) ** 2)
+        if last_loss and last_loss < loss:
+            print("Train loss: ", loss, "  WARNING - Loss Increasing")
+        else:
+            print("Train loss: ", loss)
+        last_loss = loss
+
+
+# Calculate accuracy on test data
+tes_out = sigmoid(np.dot(features_test, weights))
+predictions = tes_out > 0.5
+accuracy = np.mean(predictions == targets_test)
+print("Prediction accuracy: {:.3f}".format(accuracy))
