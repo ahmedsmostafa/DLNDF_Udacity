989dc4df472dea52253378b6200280f92e8e4453
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-06-13 02:21:38 +0200

    update nn project, remove unnecessary files

--- neurlanetworks/nn_project.py
+++ neurlanetworks/nn_project.py
@@ -97,21 +97,22 @@ class NeuralNetwork(object):
             ### Forward pass ###
             # TODO: Hidden layer - Replace these values with your calculations.
             hidden_inputs = np.dot(X, self.weights_input_to_hidden)  # signals into hidden layer
-            hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
+            hidden_outputs = self.activation_function(hidden_inputs)  # signals from hidden layer
 
             # TODO: Output layer - Replace these values with your calculations.
             final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output)  # signals into final output layer
             final_outputs = final_inputs  # signals from final output layer should be single value
-            #final_outputs = self.activation_function(hidden_inputs)
+            # final_outputs = self.activation_function(hidden_inputs)
 
             #### Implement the backward pass here ####
             ### Backward pass ###
 
             # TODO: Output error - Replace this value with your calculations.
-            error = (y-final_outputs)  # Output layer error is the difference between desired target and actual output.
+            error = (
+            y - final_outputs)  # Output layer error is the difference between desired target and actual output.
 
-            #output_error_term = error * final_outputs * (1 - final_outputs)
-            output_error_term = error * 1 #derivation of x
+            # output_error_term = error * final_outputs * (1 - final_outputs)
+            output_error_term = error * 1  # derivation of x
 
             # TODO: Calculate the hidden layer's contribution to the error
             hidden_error = np.dot(output_error_term, self.weights_hidden_to_output.T)
@@ -121,9 +122,9 @@ class NeuralNetwork(object):
             hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
 
             # Weight step (input to hidden)
-            delta_weights_i_h += hidden_error_term * X[:,None]
+            delta_weights_i_h += hidden_error_term * X[:, None]
             # Weight step (hidden to output)
-            delta_weights_h_o += output_error_term * hidden_outputs[:,None]
+            delta_weights_h_o += output_error_term * hidden_outputs[:, None]
 
         # TODO: Update the weights - Replace these values with your calculations.
         self.weights_hidden_to_output += self.lr * delta_weights_h_o / n_records  # update hidden-to-output weights with gradient descent step
@@ -147,7 +148,6 @@ class NeuralNetwork(object):
         final_outputs = final_inputs  # signals from final output layer
 
         return final_outputs
-
 
 inputs = np.array([[0.5, -0.2, 0.1]])
 targets = np.array([[0.4]])
